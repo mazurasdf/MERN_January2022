04ec87ac6efd417b5565ce7be377dfa29eac4cdf
mazurasdf <mazurasdf@gmail.com>
2022-01-11 16:32:16 -0600

    intro react lecture

--- reactIntro/first/src/App.js
+++ reactIntro/first/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const bestCharacter = "Hero";
+  const bgStyle = {
+    width: "50%",
+    height: "200px",
+    backgroundColor: "palevioletred",
+    margin: "0px auto"
+  }
+
+  const clickMe = () => {
+    console.log("clicked!!!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hey it's me!</h1>
+      <marquee onClick={()=>{alert("marquees are the best!!!!")}}><h1>weeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee!!!!!</h1></marquee>
+      <h1>8 + 6</h1>
+      <h1 style={bgStyle} onClick={clickMe}>The best character in smash ultimate is {bestCharacter}</h1>
+
+      <input type="text"/>
+
     </div>
   );
 }
