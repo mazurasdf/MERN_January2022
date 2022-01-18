6d45c451f597100f395756c69f418b789c2a93d5
mazurasdf <mazurasdf@gmail.com>
2022-01-18 08:45:17 -0600

    navigation demo

---
+++ functionalComponents/navigation/src/App.js
@@ -0,0 +1,66 @@
+import './App.css';
+import {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Step from './components/Step';
+
+function App() {
+  const [steps, setSteps] = useState([]);
+  const [direction, setDirection] = useState("left");
+  const [text, setText] = useState("");
+
+  // const displayStep = (str, i) => {
+  //   return <li>{str}</li>
+  // }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    
+    const newStep = {
+      direction,
+      text
+    };
+
+    setSteps([...steps, newStep]);
+  }
+
+  const onDeleteHandler = (index) => {
+    console.log(`trying to delete item #${index}`);
+    const newArray = [...steps];
+    newArray.splice(index,1);
+    // newArray[index].text = "edited";
+    // const newArray = [
+    //   ...steps.slice(0,index),
+    //   ...steps.slice(index+1)
+    // ]
+    
+    setSteps(newArray);
+  }
+
+  return (
+    <div className="App w-50 mx-auto">
+      <form onSubmit={onSubmitHandler}>
+        <div className="row mb-5">
+          <div className='col'>
+            <select className="form-select" onChange={(event)=>{setDirection(event.target.value)}}>
+              <option value="left">left</option>
+              <option value="right">right</option>
+              <option value="forward">forward</option>
+            </select>
+          </div>
+          <div className='col'>
+            <input type="text" placeholder="directions here..." className="form-control" onChange={(event)=>{setText(event.target.value)}}/>
+          </div>
+        </div>
+        <input type="submit" className='btn btn-success mb-4'/>
+      </form>
+
+      {
+        steps.map((step, i) => {
+          return <Step key={i} index={i} onDeleteHandler={onDeleteHandler} text={step.text} direction={step.direction} />
+        })
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ functionalComponents/navigation/src/components/Step.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Step = (props) => {
+    return(
+        <div className='step'>
+            <span>
+                {
+                    props.direction === "left" ? "⬅"
+                        : props.direction === "right" ? "➡"
+                        : "⬆"
+                }
+            </span>
+            <p>{props.text} <button onClick={()=>{props.onDeleteHandler(props.index)}}>X</button></p>
+        </div>
+    )
+}
+
+export default Step;
